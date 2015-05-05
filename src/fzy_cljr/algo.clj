a766f3fc34f65ec98ac382a27864eacba98b4c0d
Rayland <rayland.jeans@sendgrid.com>
2015-05-04 19:43:38 -0600

    Update levenshtein algorithm

--- src/fzy_cljr/algo.clj
+++ src/fzy_cljr/algo.clj
@@ -1,54 +1,67 @@
 (ns fzy-cljr.algo)
 
-(defn get-cv
-	"Gets the cell value" 
-	[same-char? prev-row cur-row col-idx]
-	(min 
-		(inc (nth prev-row col-idx))
-		(inc (last cur-row))
-		(+ (nth prev-row (dec col-idx))
-			(if same-char? 0 1))))
+(defn next-ith [p i]
+  (inc (nth p i)))
 
-(defn eq-ith
-	[word char i]
-	(= (nth word (dec i)) char))
+(defn calc-dist [same-char? prev-row i]
+  (+ (nth prev-row (dec i))
+    (if same-char? 0 1)))
+
+(defn get-min
+  "Gets the cell value" 
+  [same-char? prev-row cur-row i]
+    (min 
+      (next-ith prev-row i)
+      (inc (last cur-row))
+      (calc-dist same-char? prev-row i)
+  )
+)
+
+(defn same-char
+  "Check that the ith-1 character in word macthes
+   the char param"
+  [word char i]
+  (= (nth word (dec i)) char))
 
 (defn next-pr 
-	"Gets the next prev-row"
-	[w1 ch2 prev-row row-idx]
-	(reduce 
-  			;cur-row is an array
-  			;i is a list
-  	(fn [cur-row i]
-  		    ; set same-char to 
-  		    ;   true: if i-1th char == ch2
-  		    ;   false: otherwise
-  		    
-  		    ; Turn this into a partial
-			(let [same-char? (eq-ith w1 ch2 i)
-				    cv (get-cv same-char? prev-row cur-row i)]
-				; add to cur-row
-				;   the result of get-cv
-				(conj cur-row cv)
-			))
+  "Gets the next prev-row"
+  [w1 ch2 prev-row row-idx]
+  ;(println (str "prev-row: " prev-row))
+  (reduce 
+        ;cur-row is an array
+        ;i is a list
+    (fn [cur-row i]
+          ; set same-char to 
+          ;   true: if i-1th char == ch2
+          ;   false: otherwise
 
-  	 ; Args for anonymous fn
-  	 ; cur-row  
+          ; Turn this into a partial
+      (let [same-char? (same-char w1 ch2 i)
+            cv (get-min same-char? prev-row cur-row i)]
+        ;(println (str "w1: " w1 " cv: " cv))
+        ; add to cur-row
+        ;   the result of get-cv
+        (conj cur-row cv)
+      ))
+
+     ; Args for anonymous fn
+     ; cur-row  
       [row-idx] 
      ; i - list from 1 to prev-row count - 1
       (range 1 (count prev-row))))
 
 (defn levenshtein 
-	[w1 w2]
+  [w1 w2]
   (loop [row-idx  1
          max-rows (inc (count w2))
          prev-row (range (inc (count w1)))]
 
     ; Loop until row-idx == max-rows
     (if (= row-idx max-rows)
+      ; return the last item in prev-row
       (last prev-row)
 
-      		 ; Set ch2 to rox-idx - 1
+           ; Set ch2 to row-idx - 1
       (let [ch2 (nth w2 (dec row-idx))
             next-prev-row (next-pr w1 ch2 prev-row row-idx)]
 
