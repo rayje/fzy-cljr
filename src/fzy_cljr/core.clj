b46369df080af5bca6009e6c2b1b63fdcfd8eaf3
Rayland <rayland.jeans@sendgrid.com>
2015-05-11 06:01:19 -0600

    Add fuzzy-search

--- src/fzy_cljr/core.clj
+++ src/fzy_cljr/core.clj
@@ -1,6 +1,10 @@
 (ns fzy-cljr.core
-  (:gen-class))
+  (:gen-class)
+  (:require [fzy-cljr.algo :refer [levenshtein]]))
 
+
+(def test-words 
+  ["test" "best" "buy" "borrow" "rest" "arrow" "time" "lime" "arrow"])
 
 (defn palindrome? [s]
 	(or (<= (count s) 1)
@@ -15,6 +19,19 @@
 	(if (empty? numbers)
 		0
 		(+ (first numbers) (recursive-sum (rest numbers)))))
+
+(defn get-matches [word words]
+  (map (fn [w] (levenshtein word w)) words))
+
+(defn fuzzy-search [word words distance]
+  (doseq [w words]
+    (let [d (levenshtein word w)]
+      (if (< d distance)
+        (println w)))))
+
+(defn squares-table [numlist]
+  (doseq [num numlist]
+    (println (str "Square of " num " = " (* num num)))))
 
 (defn -main
   "Do nothing for now"
